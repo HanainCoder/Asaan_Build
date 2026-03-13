00d65202024870501b1cad804a2e30016b92f365
HanainCoder <m.hanainehsanpk@gmail.com>
2026-03-13 17:33:06 +0500

    Add Open logic

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,9 @@ import { pool } from "./db.js";
 import dotenv from "dotenv";
 import bcrypt from "bcrypt";
 import puppeteer from "puppeteer";
+import jwt from "jsonwebtoken";
 dotenv.config();
+const JWT_SECRET = process.env.JWT_SECRET || "asaanbuild_secret";
 import OpenAI from "openai";
 
 const client = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
@@ -369,10 +371,17 @@ app.post("/api/login", async (req, res) => {
       return res.json({ success: false, message: "Incorrect password" });
     }
 
+    const token = jwt.sign(
+    { id: user.id, email: user.email },
+    JWT_SECRET,
+    { expiresIn: "1d" }
+    );
+
     res.json({
-      success: true,
-      user: { id: user.id, email: user.email },
-    });
+    success: true,
+    token,
+    user: { id: user.id, email: user.email },
+   });
   } catch (err) {
     console.error("LOGIN ERROR:", err);
     res.json({ success: false, message: "Server error" });
@@ -397,6 +406,36 @@ app.get("/api/project/:id", async (req, res) => {
   } catch (err) {
     console.error(err);
     res.status(500).json({ error: "Server error" });
+  }
+});
+// Get only code of a project
+app.get("/api/project/:id/code", async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const result = await pool.query(
+      "SELECT generated_code FROM generated_projects WHERE id=$1",
+      [id]
+    );
+
+    if (result.rows.length === 0) {
+      return res.status(404).json({ 
+        success: false, 
+        message: "Project not found" 
+      });
+    }
+
+    res.json({
+      success: true,
+      code: result.rows[0].generated_code
+    });
+
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ 
+      success: false, 
+      message: "Server error" 
+    });
   }
 });
 //api to load saved code late
